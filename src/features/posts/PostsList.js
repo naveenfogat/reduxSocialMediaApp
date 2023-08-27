7f7aee4659e67b3968154f50edce72acda36d9e3
Naveen Fogat <67366731+naveenfogat@users.noreply.github.com>
2023-08-27 10:19:14 +0530

    Initial commit

---
+++ src/features/posts/PostsList.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { PostAuthor } from "./PostAuthor";
+import { TimeAgo } from "./TimeAgo";
+import { ReactionButtons } from "./ReactionButtons";
+
+export const PostsList = () => {
+  const posts = useSelector((state) => state.posts);
+  const orderedPosts = posts.slice().sort((a, b) => {
+    const dateA = a.date ?? undefined;
+    const dateB = b.date ?? undefined;
+    return dateB?.localeCompare(dateA);
+  });
+  const renderedPosts = orderedPosts.map((post) => (
+    <article className="post-excerpt" key={post.id}>
+      <h3>{post.title}</h3>
+      <PostAuthor userId={post.user} />
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+      <TimeAgo timestamp={post.date} />
+      <ReactionButtons post={post} />
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
+    </article>
+  ));
+
+  return (
+    <section className="posts-list">
+      <h2>Posts</h2>
+      {renderedPosts}
+    </section>
+  );
+};
